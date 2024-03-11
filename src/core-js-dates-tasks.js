ba00c929be9b04edee7bc7b7cce388483ffdc922
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-03-11 14:35:16 +0500

    feat: complete getCountDaysInMonth task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -114,8 +114,10 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const nextMouth = month + 1;
+  const newDate = new Date(year, month);
+  return newDate.getUTCDate()
 }
 
 /**
